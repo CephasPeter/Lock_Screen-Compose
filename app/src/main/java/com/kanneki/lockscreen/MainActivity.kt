9295b3d162e58ecc0797174bd9bb857dd96b8d0f
pearce <pearce712134@gmail.com>
2022-01-26 20:51:39 +0800

    feat init

---
+++ app/src/main/java/com/kanneki/lockscreen/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.kanneki.lockscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kanneki.lockscreen.ui.theme.LockScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LockScreenTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    PinLockScreen()
+                }
+            }
+        }
+    }
+}
+
